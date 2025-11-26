fbff465417534dfc3ecae6f28ae4a5a9386e377e
romeo <10abraham20@gmail.com>
2025-11-26 14:51:30 +0300

    commit #1
    edits to some var names

--- Products/src/com/productDetails/ProductDetails.java
+++ Products/src/com/productDetails/ProductDetails.java
@@ -3,24 +3,24 @@ package com.productDetails;
 public class ProductDetails {
     String productName;
     int price;
-    String seller;
+    String OEM;
 
-    public ProductDetails(String productName, int price, String seller) {
+    public ProductDetails(String productName, int price, String OEM) {
         this.productName = productName;
         this.price = price;
-        this.seller = seller;}
+        this.OEM= OEM;}
 
     public String getProductName() { return productName; }
 
     public int getPrice() { return price;}
 
-    public String getSeller() { return seller; }
+    public String getOEM() { return OEM; }
 
     @Override
     public String toString() {
         return "ProductDetails{" + "productName='" + getProductName() + '\'' +
                 ", price=" + getPrice() +
-                ", seller='" + getSeller() + '\'' +
+                ", seller='" + getOEM() + '\'' +
                 '}';
     }
 }
